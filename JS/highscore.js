97749e594f8f71c6fa44e0d36fa9927d0ba74054
beluchiigbo <queenbeluchi@gmail.com>
2023-09-09 17:36:42 -0500

    fix highscores js

--- JS/highscore.js
+++ JS/highscore.js
@@ -6,15 +6,15 @@ function handleEndQuiz() {
     existingScores.push(window.score);
     document.getElementById("question").innerHTML = "";
     document.getElementById("score").innerHTML = window.score;
-    document.getElementById("timer").innerHTML = "";
+    document.getElementById("timerclock").innerHTML = "";
     window.score = 0;
-    localStorage.setItem("scores", JSON.stringify(existingScores));
+    localStorage.setItem("view-scores", JSON.stringify(existingScores));
     //html scores
     for (var i = 0; i < existingScores.length; i++) {
       //show score on screen
       var listItem = document.createElement("li");
       listItem.innerHTML = existingScores[i];
-      document.getElementById("score").append(listItem);
+      document.getElementById("view-scores").append(listItem);
     }
     window.timer = 90;
     currentQuestion = 0;
@@ -26,7 +26,7 @@ function handleEndQuiz() {
       displayTimer();
       displayQuestion();
       startAgain.parentNode.removeChild(startAgain);
-      document.getElementById("score").innerHTML = "";
+      document.getElementById("view-scores").innerHTML = "";
     });
     document.querySelector("#prompt_display").appendChild(startAgain);
   }
